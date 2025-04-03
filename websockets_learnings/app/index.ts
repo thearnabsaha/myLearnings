78684e56fe7fd8d81830fa546e0dac7553b81e1f
thearnabsaha <thearnabsaha201@gmail.com>
2025-04-03 08:20:47 +0530

    feat: add WebSocket server implementation and update package dependencies

--- websockets_learnings/app/index.ts
+++ websockets_learnings/app/index.ts
@@ -1,1 +1,10 @@
-console.log("anrab saha");
+import { WebSocketServer } from "ws";
+
+const wss=new WebSocketServer({port:8080})
+wss.on("connection",(socket)=>{
+    console.log("connection stabished");
+    socket.send("hello! i am arnab saha")
+    socket.on("message",(e)=>{
+        console.log(e.toString());
+    })
+})
