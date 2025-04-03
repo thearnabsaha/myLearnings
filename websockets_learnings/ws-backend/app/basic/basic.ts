fbdd7060dad7c94775f32a00aa379ddc222c4873
thearnabsaha <thearnabsaha201@gmail.com>
2025-04-03 08:23:52 +0530

    feat: restructure WebSocket project with new backend setup and updated README

--- websockets_learnings/ws-backend/app/basic/basic.ts
+++ websockets_learnings/ws-backend/app/basic/basic.ts
@@ -3,7 +3,9 @@ import { WebSocketServer } from "ws";
 const wss=new WebSocketServer({port:8080})
 wss.on("connection",(socket)=>{
     console.log("connection stabished");
-    socket.send("hello! i am arnab saha")
+    setInterval(() => {
+        socket.send("solana price right now : "+Math.random())
+    }, 500);
     socket.on("message",(e)=>{
         console.log(e.toString());
     })
