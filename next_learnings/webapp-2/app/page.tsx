99ee28a3596a6943e05bbe6248152b51db61a299
thearnabsaha <thearnabsaha201@gmail.com>
2025-04-10 02:32:36 +0530

    feat: initialize webapp-2 with Next.js, Tailwind CSS, and Radix UI components
    - Added package.json with dependencies for Next.js, React, Tailwind CSS, and other libraries.
    - Created postcss.config.mjs for Tailwind CSS integration.
    - Added SVG assets for icons and branding.
    - Set up TypeScript configuration in tsconfig.json.
    - Implemented ThemeProvider component for theme management.
    - Developed DropdownMenu component with various subcomponents for UI interactions.

---
+++ next_learnings/webapp-2/app/page.tsx
@@ -0,0 +1,10 @@
+const Home = () => {
+  return (
+    <div className='h-screen w-screen flex flex-col justify-center items-center -translate-y-10'>
+      <h1 className='text-3xl'>You are in home page</h1>
+      <button className=' border cursor-pointer mt-5 p-5'>Go to Sign Up</button>
+    </div>
+  )
+}
+
+export default Home
--- next_learnings/webapp1/app/page.tsx
+++ next_learnings/webapp1/app/page.tsx
@@ -1,11 +1,40 @@
-"use client" // <-- tells Next.js this file should run on the client
+"use client"
+
+import * as React from "react"
+import { Moon, Sun } from "lucide-react"
+import { useTheme } from "next-themes"
 
 import { Button } from "@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
 
-export default function Home() {
+export function ModeToggle() {
+  const { setTheme } = useTheme()
+
   return (
-    <div>
-      <Button onClick={() => alert("i love you!")}>Click me</Button>
-    </div>
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline" size="icon">
+          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          <span className="sr-only">Toggle theme</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuItem onClick={() => setTheme("light")}>
+          Light
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("dark")}>
+          Dark
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("system")}>
+          System
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   )
 }
