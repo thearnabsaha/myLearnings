626c23c44e4077aa609cb5d481bd60c77452c01d
thearnabsaha <thearnabsaha201@gmail.com>
2025-04-10 20:10:47 +0530

    feat: implement C2 component with WebSocket functionality and update App structure

--- websockets_learnings/ws1/ws1-server/app/index.ts
+++ websockets_learnings/ws1/ws1-server/app/index.ts
@@ -1,43 +1,24 @@
-// import { WebSocketServer, WebSocket } from 'ws';
+import { WebSocketServer, WebSocket } from 'ws';
 
-// let allSockets: WebSocket[] = [];
-// const wss = new WebSocketServer({ port: 8080 });
-
-// wss.on('connection', (socket) => {
-//   allSockets.push(socket);
-//   console.log('Connection established');
-
-//     socket.on("message",(message)=>{
-//         try {
-//             const data=JSON.parse(message.toString())
-//             allSockets.forEach(e => {
-//                 e.send(data.username+" : "+data.message)
-//             });
-//         } catch (error) {
-//             console.log(error)
-//         }
-//     })
-
-//   socket.on('close', () => {
-//     allSockets = allSockets.filter((e) => e !== socket);
-//   });
-// });
-
-// console.log('WebSocket server is running on ws://localhost:8080');
-
-import { WebSocketServer } from 'ws';
-
+let allSockets: WebSocket[] = [];
 const wss = new WebSocketServer({ port: 3001 });
 
 wss.on('connection', (socket) => {
+  allSockets.push(socket);
   console.log('Connection established');
+  socket.send('Server is sending this message');
+
   socket.on('message', (message) => {
-    socket.send(message.toString())
+    allSockets.forEach((e) => {
+      if(e.readyState===WebSocket.OPEN){
+        e.send('server : ' + message.toString());
+      }
+    });
   });
 
-  socket.on("close", () => {
-    console.log("i got disconnected")
+  socket.on('close', () => {
+    allSockets = allSockets.filter((e) => e !== socket);
   });
 });
 
-console.log('WebSocket server is running on ws://localhost:8080');
+console.log('WebSocket server is running on ws://localhost:3001');
