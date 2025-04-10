792167368ab2b5c4bce9e8a6b77473abfb2eb564
thearnabsaha <thearnabsaha201@gmail.com>
2025-04-10 13:24:25 +0530

    feat: implement WebSocket client in C1 component and update App structure

---
+++ websockets_learnings/ws1/ws1-client/src/C1.tsx
@@ -0,0 +1,62 @@
+import { useEffect, useRef, useState } from "react"
+
+const C1 = () => {
+    const [inputValue, setInputValue] = useState('')
+    const inputRef = useRef<HTMLInputElement | null>(null)
+    const [ws, setWs] = useState<WebSocket|null>(null)
+    const [msgs, setMsgs] = useState<{msg:string,timestamps:string, from:"You"|"Server"}[]>([])
+    useEffect(() => {
+        const socket= new WebSocket("ws://localhost:3001")
+        socket.onopen=()=>{
+            console.log("socket opened")
+        }
+        socket.onmessage=(msg)=>{
+            console.log("msg : "+msg.data)
+            const time= new Date().toLocaleString()
+            setMsgs(prev=>[...prev,{msg:msg.data,timestamps:time,from:"Server"}])
+        }
+        socket.onclose=()=>{
+            console.log("socket closed")
+        }
+        socket.onerror=(error)=>{
+            console.log(error)
+        }
+        setWs(socket)
+      return () => {
+        socket.close()
+      }
+    }, [])
+    
+    const handleSend=()=>{
+        if(ws?.readyState==WebSocket.OPEN){
+            ws?.send(inputValue)
+            setInputValue("")
+            inputRef.current?.focus()
+            const time = new Date().toLocaleString()
+            setMsgs(prev=>[...prev,{msg:inputValue,timestamps:time,from:"You"}])
+        }else{
+            console.log("socket is closed")
+            setInputValue("")
+        }
+    }
+  return (
+    <div>
+        <h1>Arnab</h1>
+        <input type="text" ref={inputRef} onChange={(e)=>setInputValue(e.target.value)} value={inputValue} className="border bg-pink-50"/>
+        <button onClick={handleSend} className="border bg-blue-200 p-5 m-5">Send</button>
+        {
+            msgs.map((e,index)=>{
+                return(
+                    <div className="mb-5" key={index}>
+                        <li>{e.from}</li>
+                        <li>{e.msg}</li>
+                        <li>{e.timestamps}</li>
+                    </div>
+                )
+            })
+        }
+    </div>
+  )
+}
+
+export default C1
