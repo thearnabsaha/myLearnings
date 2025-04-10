8b219c969c14b2efd3674fbf3fc2408576e647cb
thearnabsaha <thearnabsaha201@gmail.com>
2025-04-10 11:19:31 +0530

    feat: add layout and pages for Signin and Signup with navigation

--- next_learnings/webapp-2/app/(Credentials)/signin/page.tsx
+++ next_learnings/webapp-2/app/(Credentials)/signin/page.tsx
@@ -1,14 +1,14 @@
 "use client"
 import { useRouter } from "next/navigation"
-const Home = () => {
-  const router=useRouter()
+const Signin = () => {
+  const router = useRouter()
   return (
     <div className='h-screen w-screen flex flex-col justify-center items-center -translate-y-10'>
-      <h1 className='text-3xl'>You are in home page</h1>
-      <button className=' border cursor-pointer mt-5 p-5' onClick={()=>router.push("/signup")}>Go to Sign Up</button>
+      <h1 className='text-3xl'>You are in Signin page</h1>
+      <button className=' border cursor-pointer mt-5 p-5' onClick={()=>router.push("/todos")}>Go to Todos</button>
       <button className=' border cursor-pointer mt-5 p-5' onClick={()=>router.back()}>Back</button>
     </div>
   )
 }
 
-export default Home
+export default Signin
--- next_learnings/webapp-2/app/(Credentials)/signup/page.tsx
+++ next_learnings/webapp-2/app/(Credentials)/signup/page.tsx
@@ -1,14 +1,14 @@
 "use client"
 import { useRouter } from "next/navigation"
-const Home = () => {
+const Signup = () => {
   const router=useRouter()
   return (
     <div className='h-screen w-screen flex flex-col justify-center items-center -translate-y-10'>
-      <h1 className='text-3xl'>You are in home page</h1>
-      <button className=' border cursor-pointer mt-5 p-5' onClick={()=>router.push("/signup")}>Go to Sign Up</button>
+      <h1 className='text-3xl'>You are in Signup page</h1>
+      <button className=' border cursor-pointer mt-5 p-5' onClick={()=>router.push("/signin")}>Go to Sign In</button>
       <button className=' border cursor-pointer mt-5 p-5' onClick={()=>router.back()}>Back</button>
     </div>
   )
 }
 
-export default Home
+export default Signup
