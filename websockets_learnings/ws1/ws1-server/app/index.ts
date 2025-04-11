9832411221a3c831e75bd815ce2ba81729d8fe9c
thearnabsaha <thearnabsaha201@gmail.com>
2025-04-11 14:20:41 +0530

    feat: remove unused message handling functions and types

--- websockets_learnings/ws1/ws1-server/app/index.ts
+++ websockets_learnings/ws1/ws1-server/app/index.ts
@@ -1,42 +1,57 @@
 import { WebSocketServer, WebSocket } from 'ws';
-import { broadcast, handleChat, handleJoin, handleLeave, handlePing, handleSystem } from './helper';
 
 const wss = new WebSocketServer({ port: 3001 });
-const clients=new Map<WebSocket,string>()
+const clients = new Map<WebSocket, string>()
 wss.on('connection', (socket) => {
   console.log('Connection established');
-  socket.on('message', (message) => {
-    const data=JSON.parse(message.toString())
-    switch (data.type) {
-      case "join":
-        handleJoin(data)
-        console.log("join");
-        break;
-      case "chat":
-        handleChat(data)
-        console.log("chat");
-        break;
-      case "leave":
-        handleLeave(data)
-        console.log("leave");
-        break;
-      case "ping":
-        handlePing(data)
-        console.log("ping");
-        break;
-        case "system":
-        handleSystem(data)
-        console.log("system");
-        break;
-      default:
-        socket.send("wrong type of message")
-        console.log("worng type of message")
-        break;
-    }
-  });
-  socket.on('close', () => {
-    clients.delete(socket)
-  });
+  socket.send('Server is sending this message');
+  try {
+    socket.on('message', (message) => {
+      const data=JSON.parse(message.toString())
+      const username=data.payload.username;
+      switch (data.type) {
+        case "join":
+          if(clients.get(socket)){
+            socket.send("You are already in the room")
+            return;
+          }
+          clients.set(socket,username)
+          clients.forEach((_user,KeySocket)=>{
+            if(KeySocket!==socket){
+              KeySocket.send(data.payload.username+" joined the room")
+            }
+          })
+          break;
+        case "chat":
+          if(!clients.get(socket)){
+            socket.send("You need to join first")
+            return;
+          }
+          clients.forEach((_user,keySocket)=>{
+            if(keySocket!==socket){
+              keySocket.send(clients.get(socket)+" : "+data.payload.message)
+            }
+          })
+          break;
+        case "leave":
+          console.log("leave");
+          break;
+        case "ping":
+          console.log("ping");
+          break;
+        default:
+          socket.send("wrong type of messages")
+          console.log("wrong type of messages")
+          break;
+      }
+    });
+
+    socket.on('close', () => {
+
+    });
+  } catch (error) {
+    console.log(error)
+  }
 });
 
 console.log('WebSocket server is running on ws://localhost:3001');
