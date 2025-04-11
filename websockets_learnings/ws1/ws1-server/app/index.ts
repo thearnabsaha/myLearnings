973486ec05e535e01a2278ef57f81344caa0e19b
thearnabsaha <thearnabsaha201@gmail.com>
2025-04-11 13:23:03 +0530

    feat: refactor WebSocket message handling and implement helper functions

--- websockets_learnings/ws1/ws1-server/app/index.ts
+++ websockets_learnings/ws1/ws1-server/app/index.ts
@@ -1,43 +1,39 @@
 import { WebSocketServer, WebSocket } from 'ws';
+import { broadcast, handleChat, handleJoin, handleLeave, handlePing, handleSystem } from './helper';
 
 const wss = new WebSocketServer({ port: 3001 });
 const clients=new Map<WebSocket,string>()
 wss.on('connection', (socket) => {
   console.log('Connection established');
-
   socket.on('message', (message) => {
     const data=JSON.parse(message.toString())
-    if(data.type=="join"){
-      const username=data.payload.username;
-      if(!username) return;
-      clients.set(socket,username)
-      clients.forEach((username,keySocket)=>{
-        if(keySocket.readyState===WebSocket.OPEN){
-          keySocket.send( username+" Joined Successfully")
-        }
-      })
-    }
-    if(data.type=="chat"){
-      const username=data.payload.username;
-      if(!username) return;
-      clients.forEach((username,keySocket)=>{
-        if(keySocket.readyState===WebSocket.OPEN){
-          keySocket.send( username+" left the group. ")
-        }
-      })
-    }
-    if(data.type=="leave"){
-      const username=data.payload.username;
-      if(!username) return;
-      clients.delete(socket)
-      clients.forEach((username,keySocket)=>{
-        if(keySocket.readyState===WebSocket.OPEN){
-          keySocket.send( username+" : "+data.payload.message)
-        }
-      })
+    switch (data.type) {
+      case "join":
+        handleJoin(data)
+        console.log("join");
+        break;
+      case "chat":
+        handleChat(data)
+        console.log("chat");
+        break;
+      case "leave":
+        handleLeave(data)
+        console.log("leave");
+        break;
+      case "ping":
+        handlePing(data)
+        console.log("ping");
+        break;
+        case "system":
+        handleSystem(data)
+        console.log("system");
+        break;
+      default:
+        socket.send("wrong type of message")
+        console.log("worng type of message")
+        break;
     }
   });
-
   socket.on('close', () => {
     clients.delete(socket)
   });
