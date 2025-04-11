52fef4995ff7ee77b05e105759389b5082f6e993
thearnabsaha <thearnabsaha201@gmail.com>
2025-04-11 13:23:03 +0530

    feat: refactor WebSocket message handling and implement helper functions

---
+++ websockets_learnings/ws1/ws1-server/app/helper.ts
@@ -0,0 +1,18 @@
+export const handleJoin=(data:any)=>{
+
+}
+export const handleChat=(data:any)=>{
+
+}
+export const handleLeave=(data:any)=>{
+
+}
+export const handlePing=(data:any)=>{
+
+}
+export const handleSystem=(data:any)=>{
+
+}
+export const broadcast=(data:any)=>{
+    
+}
