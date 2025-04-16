f5940abdff177a8dee6b9c511176623e1ebf0ed7
thearnabsaha <thearnabsaha201@gmail.com>
2025-04-17 02:12:38 +0530

    feat: implement sign-in and sign-up forms with validation using react-hook-form and zod

---
+++ next_learnings/webapp1/app/signup/page.tsx
@@ -0,0 +1,90 @@
+"use client"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+import { Button } from "@/components/ui/button"
+import Link from 'next/link'
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+
+const schema = z.object({
+  email: z.string().email({ message: 'Invalid email address' }),
+  username: z.string().min(3, { message: 'Username must be at least 3 characters long' }),
+  password: z
+    .string()
+    .min(8, { message: 'Password must be at least 8 characters long' })
+    .regex(/[A-Z]/, { message: 'Password must contain at least one uppercase letter' })
+    .regex(/[a-z]/, { message: 'Password must contain at least one lowercase letter' })
+    .regex(/[0-9]/, { message: 'Password must contain at least one number' })
+    .regex(/[@$!%*?&]/, { message: 'Password must contain at least one special character' }),
+});
+
+const Signup = () => {
+    const form = useForm<z.infer<typeof schema>>({
+      resolver: zodResolver(schema),
+      defaultValues: {
+        username: "",
+        email:"",
+        password:""
+      },
+    })
+    function onSubmit(values: z.infer<typeof schema>) {
+      console.log(values)
+    }
+  return (
+    <div className="w-96 border p-10 h-[400px] rounded-md">
+        <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Input placeholder="username" {...field} type="text"/>
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Input placeholder="email" {...field} type="email"/>
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Input placeholder="password" {...field} type="password"/>
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Link className="flex justify-end underline cursor-pointer" href="/signin">Sign In</Link>
+        <Button type="submit" className="w-full cursor-pointer">Submit</Button>
+      </form>
+    </Form>
+    </div>
+  )
+}
+
+export default Signup
