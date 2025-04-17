003fba134194c3f1295171d94dbf2ab63d789b20
thearnabsaha <thearnabsaha201@gmail.com>
2025-04-17 18:34:36 +0530

    feat: implement loading component and update Dashboard to display username and todos

--- next_learnings/webapp1/app/dashboard/[slug]/page.tsx
+++ next_learnings/webapp1/app/dashboard/[slug]/page.tsx
@@ -1,4 +1,16 @@
+import axios from "axios"
+
 export default async function Dashboard({params}: {params: Promise<{ slug: string }>}) {
+  const response = await axios.get<{ id: number; title: string }[]>("https://jsonplaceholder.typicode.com/todos/")
   const { slug } = await params
-  return <div>Your Username is : {slug}</div>
+  return <div>
+    <h1>Your Username is : {slug}</h1>
+    {
+      response.data.map((e)=>{
+        return(
+          <h1 key={e.id}>{e.title}</h1>
+        )
+      })
+    }
+  </div>
 }
