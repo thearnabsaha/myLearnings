f06a5bfca123ce079beebeb5cca70c236a94e624
thearnabsaha <thearnabsaha201@gmail.com>
2025-04-19 19:13:33 +0530

    feat: add theme toggle component and dropdown menu
    - Introduced ModeToggle component for theme switching using next-themes.
    - Added ThemeProvider component to wrap the application with theme context.
    - Implemented reusable Button component with variants and sizes.
    - Created DropdownMenu components for better UI interactions.
    - Updated package.json to include necessary dependencies for Radix UI and next-themes.

--- next_learnings/webapp2/app/layout.tsx
+++ next_learnings/webapp2/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
+import { ModeToggle } from "@/components/ModeToggle";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +29,19 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <div>
+              <h1 className=" text-center m-5">
+                <ModeToggle/>
+              </h1>
+            </div>
+            {children}
+        </ThemeProvider>
       </body>
     </html>
   );
