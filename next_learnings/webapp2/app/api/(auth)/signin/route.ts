a16c2e348d460c51f058270e96ec26fe13fe482a
thearnabsaha <thearnabsaha201@gmail.com>
2025-04-20 03:13:57 +0530

    feat: implement user authentication with signin and signup endpoints using Prisma

--- next_learnings/webapp2/app/api/(auth)/signin/route.ts
+++ next_learnings/webapp2/app/api/(auth)/signin/route.ts
@@ -1,7 +1,17 @@
 import { NextRequest, NextResponse } from 'next/server';
-// POST: Create data
+import { prisma } from '@/lib/prisma'
+// POST: api/signin
 export async function POST(request: NextRequest) {
   const body = await request.json();
+  const user=await prisma.user.findFirst({
+    where: {
+      username: body.username,
+    },
+  })
+  if(user){
+    
+  }
+  console.log(user)
   return NextResponse.json({
     message: 'POST: Data received successfully!',
     data: body,
