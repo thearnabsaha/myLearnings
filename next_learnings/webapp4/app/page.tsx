1c7f666c5792fc3a252959e6a4c58f20f056734b
thearnabsaha <thearnabsaha201@gmail.com>
2025-05-04 17:15:57 +0530

    feat: initialize webapp4 with Next.js, TypeScript, and Tailwind CSS
    - Added package.json with dependencies and scripts for development, build, and linting.
    - Created postcss.config.mjs for Tailwind CSS integration.
    - Added SVG assets for UI components including file, globe, next, vercel, and window icons.
    - Configured TypeScript with strict settings and module resolution for Next.js.

---
+++ next_learnings/webapp4/app/page.tsx
@@ -0,0 +1,14 @@
+import { ModeToggle } from "@/components/ModeToggle"
+import { Button } from "@/components/ui/button"
+
+const App = () => {
+  return (
+    <div>
+      App
+      <Button>Click</Button>
+      <ModeToggle/>
+    </div>
+  )
+}
+
+export default App
