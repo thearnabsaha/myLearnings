2c24f2ddc0efbe5d9ec1db97c5ec764b74f04d1c
thearnabsaha <thearnabsaha201@gmail.com>
2025-05-10 18:28:43 +0530

    chore: initialize pnpm workspace and add configuration files
    - Created pnpm-workspace.yaml to define package structure for apps and packages.
    - Added tsconfig.json extending from the workspace TypeScript configuration.
    - Introduced turbo.json for task management with build, lint, check-types, and dev tasks.

---
+++ ws2_learnings/apps/L1_Echo_Server/app/page.tsx
@@ -0,0 +1,13 @@
+import { Input } from '@workspace/ui/components/input'
+import React from 'react'
+
+const EchoServer = () => {
+  return (
+    <div>
+      EchoServer
+      <Input/>
+    </div>
+  )
+}
+
+export default EchoServer
