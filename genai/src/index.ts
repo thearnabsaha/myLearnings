4a40d3c2ab039fc45d3cef651daf330b8d1b510d
thearnabsaha <thearnabsaha201@gmail.com>
2025-08-16 13:33:50 +0530

    feat: integrate groq-sdk for chat completions and update dev script path

--- genai/src/index.ts
+++ genai/src/index.ts
@@ -7,6 +7,9 @@ import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import morgan from 'morgan';
 import helmet from 'helmet';
+import { Groq } from 'groq-sdk';
+const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
+
 
 const morganFormat = ':method :url :status :response-time ms';
 
@@ -23,8 +26,39 @@ app.use(express.json({ limit: '16kb' }));
 app.use(express.urlencoded({ extended: true, limit: '16kb' }));
 app.use(express.static('public'));
 app.use(cookieParser());
-app.get('/', (req, res) => {
-    res.send('hello from simple server :)');
+
+
+
+
+
+// app.get('/', (req, res) => {
+//     res.send('hello from simple server :)');
+// });
+app.get('/', async (req, res) => {
+    const completion = await groq.chat.completions
+        .create({
+            messages: [
+                {
+                    role: "user",
+                    content: "hi",
+                },
+            ],
+            model: "openai/gpt-oss-20b",
+        })
+        .then((chatCompletion) => {
+            // console.log(chatCompletion.choices[0]?.message?.content || "");
+            res.send(chatCompletion.choices[0]?.message?.content || "");
+        });
+});
+app.get('/health', async (req, res) => {
+    const start = Date.now();
+    const healthcheck = {
+        uptime: process.uptime(),
+        message: 'OK',
+        timestamp: new Date(),
+        responseTime: `${Date.now() - start}ms`,
+    };
+    res.status(200).json(healthcheck);
 });
 
 app.listen(port, () => console.log('> Server is up and running on port: ' + port));
