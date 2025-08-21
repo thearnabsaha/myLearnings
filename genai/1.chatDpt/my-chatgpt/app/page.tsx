7cc30e6b68f1ad3dbf6891b75a9a28f1b0be765f
thearnabsaha <thearnabsaha201@gmail.com>
2025-08-21 16:50:27 +0530

    feat: enhance chat interface with input area and message layout adjustments

--- genai/1.chatDpt/my-chatgpt/app/page.tsx
+++ genai/1.chatDpt/my-chatgpt/app/page.tsx
@@ -1,36 +1,43 @@
 import { ModeToggle } from '@/components/ModeToggle'
-import { MessageCircleDashed } from 'lucide-react'
+import { ArrowRight, ArrowUp, MessageCircleDashed } from 'lucide-react'
 import React from 'react'
-
+import { Textarea } from "@/components/ui/textarea"
+import { Button } from "@/components/ui/button"
 const page = () => {
   return (
     <div className='mx-auto w-[50vw]'>
-      <div className='flex items-center justify-between mt-2'>
+      <div className='flex items-center justify-between mt-2 fixed w-screen left-0 px-10'>
         <div className='flex items-center'>
           <MessageCircleDashed />
           <h1 className='text-2xl'>TempChat</h1>
         </div>
         <ModeToggle />
       </div >
-      <div>
-        <div className='flex flex-col flex-wrap'>
-          <p className='font-light py-1.5 px-3 rounded-xl bg-accent my-5 max-w-56 self-end'>Arnab is the best person in the world</p>
-          <p className='font-light py-1.5 px-3 rounded-xl my-5 self-start'>Arnab is the best person in the world</p>
-          <p className='font-light py-1.5 px-3 rounded-xl bg-accent my-5 max-w-56 self-end'>Lorem ipsum dolor sit amet.</p>
-          <p className='font-light py-1.5 px-3 rounded-xl my-5 self-start'>Lorem ipsum dolor sit amet.</p>
-          <p className='font-light py-1.5 px-3 rounded-xl bg-accent my-5 max-w-56 self-end'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Libero minus distinctio commodi dignissimos perferendis ab necessitatibus nesciunt praesentium accusantium. Consequatur cum nam nulla error recusandae?</p>
-          <p className='font-light py-1.5 px-3 rounded-xl my-5 self-start'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Libero minus distinctio commodi dignissimos perferendis ab necessitatibus nesciunt praesentium accusantium. Consequatur cum nam nulla error recusandae?</p>
-          <p className='font-light py-1.5 px-3 rounded-xl bg-accent my-5 max-w-56 self-end'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat corrupti assumenda nihil tenetur provident laudantium, dignissimos dolorem quae culpa accusamus impedit omnis nulla animi recusandae enim mollitia dolor nam expedita.</p>
-          <p className='font-light py-1.5 px-3 rounded-xl my-5 self-start'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat corrupti assumenda nihil tenetur provident laudantium, dignissimos dolorem quae culpa accusamus impedit omnis nulla animi recusandae enim mollitia dolor nam expedita.</p>
-          <p className='font-light py-1.5 px-3 rounded-xl bg-accent my-5 max-w-56 self-end'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque, perferendis?</p>
-          <p className='font-light py-1.5 px-3 rounded-xl my-5 self-start'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque, perferendis?</p>
-          <p className='font-light py-1.5 px-3 rounded-xl bg-accent my-5 max-w-56 self-end'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illo vitae provident aliquid sint consequatur, eos sit eaque repudiandae itaque nihil numquam accusantium, porro, alias assumenda.</p>
-          <p className='font-light py-1.5 px-3 rounded-xl my-5 self-start'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illo vitae provident aliquid sint consequatur, eos sit eaque repudiandae itaque nihil numquam accusantium, porro, alias assumenda.</p>
-          <p className='font-light py-1.5 px-3 rounded-xl bg-accent my-5 max-w-56 self-end'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore dolore, nobis in ullam dicta numquam.</p>
-          <p className='font-light py-1.5 px-3 rounded-xl my-5 self-start'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore dolore, nobis in ullam dicta numquam.</p>
-        </div>
+      <div className='flex flex-col flex-wrap mb-30'>
+        <p className='font-light py-1.5 px-3 rounded-xl bg-accent my-5 max-w-72 self-end'>Arnab is the best person in the world</p>
+        <p className='font-light py-1.5 px-3 rounded-xl my-5 self-start'>Arnab is the best person in the world</p>
+        <p className='font-light py-1.5 px-3 rounded-xl bg-accent my-5 max-w-72 self-end'>Lorem ipsum dolor sit amet.</p>
+        <p className='font-light py-1.5 px-3 rounded-xl my-5 self-start'>Lorem ipsum dolor sit amet.</p>
+        <p className='font-light py-1.5 px-3 rounded-xl bg-accent my-5 max-w-72 self-end'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Libero minus distinctio commodi dignissimos perferendis ab necessitatibus nesciunt praesentium accusantium. Consequatur cum nam nulla error recusandae?</p>
+        <p className='font-light py-1.5 px-3 rounded-xl my-5 self-start'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Libero minus distinctio commodi dignissimos perferendis ab necessitatibus nesciunt praesentium accusantium. Consequatur cum nam nulla error recusandae?</p>
+        <p className='font-light py-1.5 px-3 rounded-xl bg-accent my-5 max-w-72 self-end'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat corrupti assumenda nihil tenetur provident laudantium, dignissimos dolorem quae culpa accusamus impedit omnis nulla animi recusandae enim mollitia dolor nam expedita.</p>
+        <p className='font-light py-1.5 px-3 rounded-xl my-5 self-start'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat corrupti assumenda nihil tenetur provident laudantium, dignissimos dolorem quae culpa accusamus impedit omnis nulla animi recusandae enim mollitia dolor nam expedita.</p>
+        <p className='font-light py-1.5 px-3 rounded-xl bg-accent my-5 max-w-72 self-end'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque, perferendis?</p>
+        <p className='font-light py-1.5 px-3 rounded-xl my-5 self-start'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque, perferendis?</p>
+        <p className='font-light py-1.5 px-3 rounded-xl bg-accent my-5 max-w-72 self-end'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illo vitae provident aliquid sint consequatur, eos sit eaque repudiandae itaque nihil numquam accusantium, porro, alias assumenda.</p>
+        <p className='font-light py-1.5 px-3 rounded-xl my-5 self-start'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illo vitae provident aliquid sint consequatur, eos sit eaque repudiandae itaque nihil numquam accusantium, porro, alias assumenda.</p>
+        <p className='font-light py-1.5 px-3 rounded-xl bg-accent my-5 max-w-72 self-end'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore dolore, nobis in ullam dicta numquam.</p>
+        <p className='font-light py-1.5 px-3 rounded-xl my-5 self-start'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore dolore, nobis in ullam dicta numquam.</p>
       </div>
-    </div>
+      <div className='fixed bottom-0 py-10 left-0 w-screen flex justify-center bg-background'>
+        <div className='w-[40vw] flex rounded-2xl'>
+          {/* <Textarea className='rounded-l-2xl rounded-r-none h-10 resize-none' placeholder="Ask anything" /> */}
+          {/* <Button className='cursor-pointer rounded-l-none h-10 rounded-r-2xl'><ArrowRight /></Button> */}
+          <Textarea className='rounded-l-2xl rounded-r-none h-10 resize-none' placeholder="Ask anything" />
+          <Button className='cursor-pointer rounded-l-none h-10 rounded-r-2xl'><ArrowRight /></Button>
+        </div>
+      </div >
+    </div >
   )
 }
 
