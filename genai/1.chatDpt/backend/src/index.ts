479949f373ddd92048cf3baf610073593a13cc75
thearnabsaha <thearnabsaha201@gmail.com>
2025-08-21 23:15:30 +0530

    refactor: consolidate backend setup by removing unused files and integrating middleware

--- genai/1.chatDpt/backend/src/index.ts
+++ genai/1.chatDpt/backend/src/index.ts
@@ -1,90 +1,54 @@
-import readline from 'node:readline/promises'
+import express from 'express';
+const app = express();
 import dotenv from 'dotenv';
 dotenv.config();
+const port = process.env.PORT || 3001;
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import morgan from 'morgan';
+import helmet from 'helmet';
 import { Groq } from 'groq-sdk';
 import { tavily } from "@tavily/core";
 const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
 const tvly = tavily({ apiKey: process.env.TAVILY_API_KEY });
-let messages = [
+
+const morganFormat = ':method :url :status :response-time ms';
+
+app.use(morgan(morganFormat));
+app.use(helmet());
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+}));
+
+app.use(express.json({ limit: '16kb' }));
+app.use(express.urlencoded({ extended: true, limit: '16kb' }));
+app.use(express.static('public'));
+app.use(cookieParser());
+
+const messages = [
     {
         role: "system",
-        content: `You are a personal assistent, who answers the asked questions, who gives answers in one word or sentence.
-                    Current date and time is: ${new Date().toUTCString()}
-                    You have access to following tools:
-                    1. webSearch({query}:{query:string}) //Search the latest information and the realtime data on the internet
-                    `
+        content: "You are a helpful assistant.",
     },
-
+    {
+        role: "user",
+        content: "hello",
+    },
 ]
-const webSearch = async ({ query }: { query: string }) => {
-    console.log("webSearch tool is getting called...")
-    const response = await tvly.search(query);
-    const responseContents = response.results.map((e) => e.content).join("\n\n")
-    return responseContents
-}
-async function main() {
-    const rl = readline.createInterface({ input: process.stdin, output: process.stdout })
-    while (true) {
-        let question = await rl.question("You : ")
-        messages.push({
-            role: "user",
-            content: String(question)
-        })
-        if (question === "bye") {
-            break;
-        }
-        while (true) {
-            const completion = await groq.chat.completions
-                .create({
-                    temperature: 0,
-                    //@ts-ignore
-                    messages: messages,
-                    model: "openai/gpt-oss-20b",
-                    tools: [
-                        {
-                            type: "function",
-                            function: {
-                                name: "webSearch",
-                                description: "Search the latest information and the realtime data on the internet",
-                                parameters: {
-                                    type: "object",
-                                    properties: {
-                                        query: {
-                                            type: "string",
-                                            description: "The search query to perform search on"
-                                        },
-                                    },
-                                    required: ["query"]
-                                }
-                            }
-                        }
-                    ],
-                    tool_choice: "auto"
-                })
-            //@ts-ignore
-            messages.push(completion.choices[0]?.message)
-            let toolResult;
-            const toolcalls = await completion.choices[0]?.message.tool_calls
-            if (!toolcalls) {
-                console.log("Rose :", completion.choices[0]?.message?.content)
-                break;
-            }
-            for (const e of toolcalls) {
-                const functionName = e.function.name
-                const functionArguments = e.function.arguments
-                if (functionName === "webSearch") {
-                    toolResult = await webSearch(JSON.parse(functionArguments))
-                    messages.push({
-                        // @ts-ignore
-                        tool_call_id: e.id,
-                        role: "tool",
-                        name: functionName,
-                        content: toolResult
-                    })
-                }
-            }
-        }
-    }
-    rl.close()
-}
-main()
+export const getGroqChatCompletion = async () => {
+    return groq.chat.completions.create({
+        //@ts-ignore
+        messages: messages,
+        model: "openai/gpt-oss-20b",
+    });
+};
+
+app.get('/', async (req, res) => {
+    const completion = await getGroqChatCompletion();
+    console.log(completion.choices[0]?.message?.content || "");
+    res.send('hello from simple server :)');
+});
+
+app.listen(port, () => console.log('> Server is up and running on port: ' + port));
