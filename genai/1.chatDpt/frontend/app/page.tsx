c87c9744485bf8221bf8a441ad982577f0008495
thearnabsaha <thearnabsaha201@gmail.com>
2025-08-21 23:24:46 +0530

    feat: implement form components with validation using react-hook-form and zod

--- genai/1.chatDpt/frontend/app/page.tsx
+++ genai/1.chatDpt/frontend/app/page.tsx
@@ -1,9 +1,38 @@
+"use client"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
 import { ModeToggle } from '@/components/ModeToggle'
 import { ArrowUp, MessageCircleDashed } from 'lucide-react'
 import React from 'react'
 import { Textarea } from "@/components/ui/textarea"
 import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+
+import { z } from "zod"
+const formSchema = z.object({
+  username: z.string().min(1)
+})
 const page = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    },
+  })
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values)
+    form.reset()
+  }
   return (
     <div className='mx-auto w-[50vw]'>
       <div className='flex items-center justify-between mt-2 fixed w-screen left-0 px-10'>
@@ -30,10 +59,23 @@ const page = () => {
         <p className='font-light py-1.5 px-3 rounded-xl my-5 self-start'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore dolore, nobis in ullam dicta numquam.</p>
       </div>
       <div className='fixed bottom-0 py-10 left-0 w-screen flex justify-center bg-background'>
-        <div className='w-[40vw] flex rounded-2xl bg-accent items-center p-3'>
-          <Textarea className='max-h-60 resize-none border-none mr-2' placeholder="Ask anything" />
-          <Button className='cursor-pointer rounded-4xl h-8 w-8 mr-2'><ArrowUp /></Button>
-        </div>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className='w-[40vw] flex rounded-2xl bg-accent items-center p-3'>
+            <FormField
+              control={form.control}
+              name="username"
+              render={({ field }) => (
+                <FormItem className="w-full">
+                  <FormControl>
+                    <Textarea className='max-h-60 resize-none border-none mr-2' placeholder="Ask anything" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button className='cursor-pointer rounded-4xl h-8 w-8 mr-2' type="submit"><ArrowUp /></Button>
+          </form>
+        </Form>
       </div >
     </div >
   )
