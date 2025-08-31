ba18284a837d0f0fbcf8c62edb72992d47eb1ac1
thearnabsaha <thearnabsaha201@gmail.com>
2025-08-31 20:13:17 +0530

    feat: implement chat agent functionality with LangChain integration

--- genai/3.chatLpt/backend/src/agent.ts
+++ genai/3.chatLpt/backend/src/agent.ts
@@ -1,32 +1,38 @@
-// import { StateGraph, MessagesAnnotation } from "@langchain/langgraph";
-// import { createReactAgent } from "@langchain/langgraph/prebuilt";
-// import { TavilySearch } from "@langchain/tavily";
-// import { ToolNode } from "@langchain/langgraph/prebuilt";
-// import { ChatGroq } from "@langchain/groq";
-// export const agent = async () => {
-//     const search = new TavilySearch({
-//         maxResults: 5,
-//         topic: "general",
-//     });
-//     const tools = [search];
-//     const toolNode = new ToolNode(tools);
-//     const model = new ChatGroq({
-//         model: "openai/gpt-oss-20b",
-//         temperature: 0,
-//     }).bindTools(tools);
-//     const agent = await createReactAgent({
-//         llm: model,
-//         tools: [search],
-//     });
+import { StateGraph, MessagesAnnotation } from "@langchain/langgraph";
+import { createReactAgent } from "@langchain/langgraph/prebuilt";
+import { TavilySearch } from "@langchain/tavily";
+import { ToolNode } from "@langchain/langgraph/prebuilt";
+import { ChatGroq } from "@langchain/groq";
+import { HumanMessage, AIMessage } from "@langchain/core/messages";
+import dotenv from 'dotenv';
+dotenv.config();
+export const agent = async () => {
+    const search = new TavilySearch({
+        maxResults: 5,
+        topic: "general",
+    });
+    const tools = [search];
+    const toolNode = new ToolNode(tools);
+    const model = new ChatGroq({
+        model: "openai/gpt-oss-20b",
+        temperature: 0,
+    }).bindTools(tools);
+    const agent = await createReactAgent({
+        llm: model,
+        tools: [search],
+    });
 
-// }
-// // // Define a new graph
-// // const workflow = new StateGraph(MessagesAnnotation)
-// // // .addNode("agent", callModel)
-// // // .addEdge("__start__", "agent") // __start__ is a special name for the entrypoint
-// // // .addNode("tools", toolNode)
-// // // .addEdge("tools", "agent")
-// // // .addConditionalEdges("agent", shouldContinue);
+    // Define a new graph
+    const workflow = new StateGraph(MessagesAnnotation)
+        .addNode("agent", agent)
+        .addEdge("__start__", "agent")
+        .addEdge("agent", "__end__")
+    const app = workflow.compile();
+    // Use the agent
+    const finalState = await app.invoke({
+        messages: [new HumanMessage("what is the weather in sf")],
+    });
+    console.log(finalState.messages[finalState.messages.length - 1].content);
 
-// // // Finally, we compile it into a LangChain Runnable.
-// // const app = workflow.compile();
+}
+agent()
