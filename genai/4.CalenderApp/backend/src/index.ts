515e90d9d9f49c780fcb4030b880a9b5ea6a5d9b
thearnabsaha <thearnabsaha201@gmail.com>
2025-09-01 18:27:15 +0530

    feat: update dependencies and refactor chat functionality in backend chore: remove unused code in frontend and replace with landing page

--- genai/4.CalenderApp/backend/src/index.ts
+++ genai/4.CalenderApp/backend/src/index.ts
@@ -7,7 +7,8 @@ import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import morgan from 'morgan';
 import helmet from 'helmet';
-import { agent } from './agent';
+// import { agent } from './agent';
+import { prisma } from './utils/prisma';
 
 const morganFormat = ':method :url :status :response-time ms';
 
@@ -21,11 +22,17 @@ app.use(express.urlencoded({ extended: true, limit: '16kb' }));
 app.use(express.static('public'));
 app.use(cookieParser());
 
-app.post('/chat', async (req, res) => {
-    const inputMessage = req.body.inputMessage as string
-    const threadId = req.body.threadId as string
-    const answer = await agent(inputMessage, threadId)
-    res.send(answer);
+// app.post('/chat', async (req, res) => {
+//     const inputMessage = req.body.inputMessage as string
+//     const threadId = req.body.threadId as string
+//     const answer = await agent(inputMessage, threadId)
+//     res.send(answer);
+// });
+app.post('/signup', async (req, res) => {
+    const user = await prisma.user.createMany({
+        data: req.body
+    });
+    res.send(user);
 });
 
 app.listen(port, () => console.log('> Server is up and running on port: ' + port));
