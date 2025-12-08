e816c03f50579ed9329ce49d36a832844ad00ee9
thearnabsaha <thearnabsaha201@gmail.com>
2025-12-08 19:20:26 +0530

    feat: initialize backend with Express, add middleware and basic server setup

---
+++ genai/7. ComedyTweetWriter/BackendBun/index.ts
@@ -0,0 +1,37 @@
+import express from 'express';
+const app = express();
+import dotenv from 'dotenv';
+dotenv.config();
+const port = process.env.PORT || 3000;
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import morgan from 'morgan';
+import helmet from 'helmet';
+
+// npm install express cors cookie-parser dotenv helmet morgan
+// npm install -D @types/express @types/cors @types/cookie-parser @types/morgan
+
+const morganFormat = ':method :url :status :response-time ms';
+
+app.use(morgan(morganFormat));
+app.use(helmet());
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+}));
+
+// app.options('*', cors({
+//     origin: process.env.CORS_ORIGIN,
+//     credentials: true,
+// }));
+
+app.use(express.json({ limit: '16kb' }));
+app.use(express.urlencoded({ extended: true, limit: '16kb' }));
+app.use(express.static('public'));
+app.use(cookieParser());
+app.get('/', (req, res) => {
+    res.send('hello from simple server :)');
+});
+
+app.listen(port, () => console.log('> Server is up and running on port: ' + port));
