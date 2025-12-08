abd215975fc01761dd1639304fa8583781dd66ed
thearnabsaha <thearnabsaha201@gmail.com>
2025-12-08 20:16:52 +0530

    feat: update LLM prompts and restructure graph nodes for Twitter writing and reviewing

--- genai/7. ComedyTweetWriter/BackendBun/src/graph.ts
+++ genai/7. ComedyTweetWriter/BackendBun/src/graph.ts
@@ -1,7 +1,8 @@
 import { MessagesAnnotation, StateGraph, START, END } from "@langchain/langgraph";
 import { ChatGroq } from "@langchain/groq";
 import { AIMessage, HumanMessage } from "langchain";
-import { firstPrompt } from "./prompt";
+import { SystemMessage } from '@langchain/core/messages';
+import { TwitterReviewerPrompt, TwitterWriterPrompt } from "./prompt";
 export const callLLM = async () => {
     const model = new ChatGroq({
         model: "openai/gpt-oss-20b",
@@ -12,13 +13,23 @@ export const callLLM = async () => {
         const response = await model.invoke(state.messages);
         return { messages: [...state.messages, response] };
     };
+    const writer = async (state: typeof MessagesAnnotation.State) => {
+        const response = await model.invoke([{ role: "system", content: TwitterWriterPrompt }, ...state.messages]);
+        return { messages: [...state.messages, response] };
+    };
+    const reviewer = async (state: typeof MessagesAnnotation.State) => {
+        const response = await model.invoke([{ role: "system", content: TwitterReviewerPrompt }, ...state.messages]);
+        return { messages: [...state.messages, response] };
+    };
 
     const graph = new StateGraph(MessagesAnnotation)
-        .addNode("LLM", LLM)
-        .addEdge(START, "LLM")
-        .addEdge("LLM", END)
+        .addNode("writer", writer)
+        // .addNode("reviewer", reviewer)
+        .addEdge(START, "writer")
+        // .addConditionalEdges('writer',"reviewer",)
+        // .addEdge("writer", END)
         .compile();
 
-    const answer = await graph.invoke({ messages: [new AIMessage(firstPrompt), new HumanMessage("hi! I am arnab")] });
+    const answer = await graph.invoke({ messages: [new HumanMessage("write a tweet about bunjs")] });
     console.log(answer.messages[answer.messages.length - 1].content)
 }
