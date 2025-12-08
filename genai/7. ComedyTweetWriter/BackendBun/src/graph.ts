051c3b20d09f9fff33e2b5021925bd7680c64788
thearnabsaha <thearnabsaha201@gmail.com>
2025-12-08 20:02:58 +0530

    feat: refactor LLM integration in graph and add firstPrompt definition

--- genai/7. ComedyTweetWriter/BackendBun/src/graph.ts
+++ genai/7. ComedyTweetWriter/BackendBun/src/graph.ts
@@ -1,23 +1,24 @@
 import { MessagesAnnotation, StateGraph, START, END } from "@langchain/langgraph";
 import { ChatGroq } from "@langchain/groq";
-import { HumanMessage } from "langchain";
+import { AIMessage, HumanMessage } from "langchain";
+import { firstPrompt } from "./prompt";
 export const callLLM = async () => {
     const model = new ChatGroq({
         model: "openai/gpt-oss-20b",
         temperature: 0
     });
 
-    const mockLlm = async (state: typeof MessagesAnnotation.State) => {
+    const LLM = async (state: typeof MessagesAnnotation.State) => {
         const response = await model.invoke(state.messages);
         return { messages: [...state.messages, response] };
     };
 
     const graph = new StateGraph(MessagesAnnotation)
-        .addNode("mock_llm", mockLlm)
-        .addEdge(START, "mock_llm")
-        .addEdge("mock_llm", END)
+        .addNode("LLM", LLM)
+        .addEdge(START, "LLM")
+        .addEdge("LLM", END)
         .compile();
 
-    const answer = await graph.invoke({ messages: [{ role: "user", content: "hi!" }] });
-    console.log(answer)
+    const answer = await graph.invoke({ messages: [new AIMessage(firstPrompt), new HumanMessage("hi! I am arnab")] });
+    console.log(answer.messages[answer.messages.length - 1].content)
 }
---
+++ genai/7. ComedyTweetWriter/BackendBun/src/prompt.ts
@@ -0,0 +1,1 @@
+export const firstPrompt = "you are a sweet girl and you love arnab, you always let that know to arnab not others, but with others you talk rudely!"
