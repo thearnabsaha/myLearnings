bd040d7f2ab7dfc51dc0850b5e8a227ab2b910ab
thearnabsaha <thearnabsaha201@gmail.com>
2025-12-09 00:44:31 +0530

    refactor: update model configuration and improve graph structure in agent function

--- genai/8. PromptEnhancer/backend/src/graph.ts
+++ genai/8. PromptEnhancer/backend/src/graph.ts
@@ -3,32 +3,33 @@ import { ChatGroq } from "@langchain/groq";
 import { HumanMessage } from "langchain";
 import { StateAnnotation } from "./state";
 import { PromptEnhancerPrompt, PromptEnhancerReviewerPrompt } from "./prompt";
-export const agent = async (inputMessage: string, threadId: string) => {
-    const model = new ChatGroq({
-        model: "openai/gpt-oss-20b",
-        temperature: 0
-    });
-    const writer = async (state: typeof StateAnnotation.State) => {
-        const response = await model.invoke([{ role: "system", content: PromptEnhancerPrompt }, ...state.messages]);
-        return { messages: [response], iteration: Number(state.iteration) >= 1 ? state.iteration : 1 };
-    };
-    const reviewer = async (state: typeof StateAnnotation.State) => {
-        const response = await model.invoke([{ role: "system", content: PromptEnhancerReviewerPrompt }, ...state.messages]);
-        return { messages: [new HumanMessage(response.content as string)], iteration: Number(state.iteration) + 1 };
-    };
-    const nextNode = (state: typeof StateAnnotation.State) => {
-        if (Number(state.iteration) < 2) {
-            return "reviewer"
-        }
-        return END
+const model = new ChatGroq({
+    model: "openai/gpt-oss-120b",
+    temperature: 0
+});
+const writer = async (state: typeof StateAnnotation.State) => {
+    const response = await model.invoke([{ role: "system", content: PromptEnhancerPrompt }, ...state.messages]);
+    return { messages: [response], iteration: Number(state.iteration) >= 1 ? state.iteration : 1 };
+};
+const reviewer = async (state: typeof StateAnnotation.State) => {
+    const response = await model.invoke([{ role: "system", content: PromptEnhancerReviewerPrompt }, ...state.messages]);
+    return { messages: [new HumanMessage(response.content as string)], iteration: Number(state.iteration) + 1 };
+};
+const nextNode = (state: typeof StateAnnotation.State) => {
+    if (Number(state.iteration) < 2) {
+        return "reviewer"
     }
-    const graph = new StateGraph(StateAnnotation)
-        .addNode("writer", writer)
-        .addNode("reviewer", reviewer)
-        .addEdge(START, "writer")
-        .addConditionalEdges('writer', nextNode)
-        .addEdge("reviewer", "writer")
-        .compile();
+    return END
+}
+const graph = new StateGraph(StateAnnotation)
+    .addNode("writer", writer)
+    .addNode("reviewer", reviewer)
+    .addEdge(START, "writer")
+    .addConditionalEdges('writer', nextNode)
+    .addEdge("reviewer", "writer")
+    .compile();
+
+export const agent = async (inputMessage: string, threadId: string) => {
 
     const answer = await graph.invoke({ messages: [new HumanMessage(inputMessage)] }, { configurable: { thread_id: threadId } },);
     console.log(answer)
