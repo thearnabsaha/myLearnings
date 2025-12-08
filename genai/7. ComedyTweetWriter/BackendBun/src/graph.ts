5a9d0295313780c9bcb8e6485ca9a82705eeb8e6
thearnabsaha <thearnabsaha201@gmail.com>
2025-12-08 19:47:34 +0530

    feat: update langchain dependency and refactor LLM invocation in graph

--- genai/7. ComedyTweetWriter/BackendBun/src/graph.ts
+++ genai/7. ComedyTweetWriter/BackendBun/src/graph.ts
@@ -1,21 +1,23 @@
 import { MessagesAnnotation, StateGraph, START, END } from "@langchain/langgraph";
-
-// const mockLlm = (state: typeof MessagesAnnotation.State) => {
-//     return { messages: [{ role: "ai", content: "hello world" }] };
-// };
 import { ChatGroq } from "@langchain/groq";
+import { HumanMessage } from "langchain";
 export const callLLM = async () => {
     const model = new ChatGroq({
         model: "openai/gpt-oss-20b",
         temperature: 0
     });
-    // const graph = new StateGraph(MessagesAnnotation)
-    //     .addNode("mock_llm", model)
-    //     .addEdge(START, "mock_llm")
-    //     .addEdge("mock_llm", END)
-    //     .compile();
 
-    // await graph.invoke({ messages: [{ role: "user", content: "hi!" }] });
-    const answer = await model.invoke("Hello, I am arnab!")
-    console.log(answer.content)
+    const mockLlm = async (state: typeof MessagesAnnotation.State) => {
+        const response = await model.invoke(state.messages);
+        return { messages: [...state.messages, response] };
+    };
+
+    const graph = new StateGraph(MessagesAnnotation)
+        .addNode("mock_llm", mockLlm)
+        .addEdge(START, "mock_llm")
+        .addEdge("mock_llm", END)
+        .compile();
+
+    const answer = await graph.invoke({ messages: [{ role: "user", content: "hi!" }] });
+    console.log(answer)
 }
