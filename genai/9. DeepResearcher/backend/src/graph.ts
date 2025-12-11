1628523f1e2be7cda9039023150ad9bdd42a70d2
thearnabsaha <thearnabsaha201@gmail.com>
2025-12-11 15:13:47 +0530

    feat: add initial frontend setup with SVG icons and TypeScript configuration

---
+++ genai/9. DeepResearcher/backend/src/graph.ts
@@ -0,0 +1,41 @@
+import { StateGraph, START, END } from "@langchain/langgraph";
+import { ChatGroq } from "@langchain/groq";
+import { HumanMessage } from "langchain";
+import { StateAnnotation } from "./state";
+import { PromptEnhancerPrompt, PromptEnhancerReviewerPrompt } from "./prompt";
+
+const model = new ChatGroq({
+    model: "openai/gpt-oss-120b",
+    temperature: 0
+});
+
+// const writer = async (state: typeof StateAnnotation.State) => {
+//     const response = await model.invoke([{ role: "system", content: PromptEnhancerPrompt }, ...state.messages]);
+//     // console.log(state.messages)
+//     return { messages: [response], iteration: Number(state.iteration) >= 1 ? state.iteration : 1 };
+// };
+// const reviewer = async (state: typeof StateAnnotation.State) => {
+//     const response = await model.invoke([{ role: "system", content: PromptEnhancerReviewerPrompt }, ...state.messages]);
+//     return { messages: [new HumanMessage(response.content as string)], iteration: Number(state.iteration) + 1 };
+// };
+// const nextNode = (state: typeof StateAnnotation.State) => {
+//     if (Number(state.iteration) < 2) {
+//         return "reviewer"
+//     }
+//     return END
+// }
+
+const graph = new StateGraph(StateAnnotation)
+    // .addNode("writer", writer)
+    // .addNode("reviewer", reviewer)
+    // .addEdge(START, "writer")
+    // .addConditionalEdges('writer', nextNode)
+    // .addEdge("reviewer", "writer")
+    .compile();
+
+export const agent = async (inputMessage: string, threadId: string) => {
+    const answer = await graph.invoke({ messages: [new HumanMessage(inputMessage)] }, { configurable: { thread_id: threadId } },);
+    // console.log(answer)
+    // console.log(answer.messages[answer.messages.length - 1].content)
+    return answer.messages[answer.messages.length - 1].content
+}
