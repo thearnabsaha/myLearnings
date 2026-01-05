72b513429260758facd8fc06affb91bf24432647
thearnabsaha <thearnabsaha201@gmail.com>
2026-01-05 22:16:54 +0530

    feat: add initial frontend setup with SVG assets and TypeScript configuration

---
+++ genai/10. PerfectAgentAI/backend/src/graph.ts
@@ -0,0 +1,11 @@
+
+
+// Invoke
+export const agent = async (inputMessage: string, threadId: string) => {
+    // const result = await graph.invoke({
+    //     messages: [new HumanMessage(inputMessage)],
+    // });
+    // console.log(result)
+    // console.log(result.messages[result.messages.length - 1].content)
+    // return result.messages[result.messages.length - 1].content
+}
